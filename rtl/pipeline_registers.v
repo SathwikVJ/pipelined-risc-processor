447976ff200b24aef12bcbcb6957061c07a27d46
SathwikVJ <sathwikvempaty@gmail.com>
2026-03-11 18:53:18 +0530

    Update pipeline_registers.v

--- rtl/pipeline_registers.v
+++ rtl/pipeline_registers.v
@@ -1,26 +1,26 @@
 module pipeline_registers(
-input clk,
-input reset,
+    input clk,
+    input reset,
 
-input [7:0] if_id_in,
-output reg [7:0] if_id_out,
+    input [7:0] if_id_in,
+    output reg [7:0] if_id_out,
 
-input [7:0] id_ex_in,
-output reg [7:0] id_ex_out,
+    input [7:0] id_ex_in,
+    output reg [7:0] id_ex_out,
 
-input [7:0] ex_mem_in,
-output reg [7:0] ex_mem_out,
+    input [7:0] ex_mem_in,
+    output reg [7:0] ex_mem_out,
 
-input [7:0] mem_wb_in,
-output reg [7:0] mem_wb_out
+    input [7:0] mem_wb_in,
+    output reg [7:0] mem_wb_out
 );
 
 always @(posedge clk or posedge reset) begin
     if(reset) begin
-        if_id_out <= 0;
-        id_ex_out <= 0;
-        ex_mem_out <= 0;
-        mem_wb_out <= 0;
+        if_id_out <= 8'b00000000;
+        id_ex_out <= 8'b00000000;
+        ex_mem_out <= 8'b00000000;
+        mem_wb_out <= 8'b00000000;
     end
     else begin
         if_id_out <= if_id_in;
