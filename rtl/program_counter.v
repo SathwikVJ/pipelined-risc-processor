c35de00c1b344f36e6493764f89ade496e7cd498
SathwikVJ <sathwikvempaty@gmail.com>
2026-03-11 17:17:05 +0530

    Update program_counter.v

--- rtl/program_counter.v
+++ rtl/program_counter.v
@@ -1,1 +1,14 @@
+module program_counter(
+input clk,
+input reset,
+output reg [7:0] pc
+);
 
+always @(posedge clk or posedge reset) begin
+if(reset)
+pc <= 0;
+else
+pc <= pc + 1;
+end
+
+endmodule
