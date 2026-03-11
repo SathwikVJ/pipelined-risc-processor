acc98b13c4e4c4c1758c8bb0898fc14564f04a94
SathwikVJ <sathwikvempaty@gmail.com>
2026-03-11 19:30:33 +0530

    Update alu.v

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,19 +1,18 @@
 module alu(
-input [7:0] a,
-input [7:0] b,
-input [2:0] alu_op,
-output reg [7:0] result
+    input  [7:0] a,
+    input  [7:0] b,
+    input  [1:0] alu_control,
+    output reg [7:0] result
 );
 
 always @(*) begin
-case(alu_op)
-3'b000: result = a + b;
-3'b001: result = a - b;
-3'b010: result = a & b;
-3'b011: result = a | b;
-3'b100: result = a ^ b;
-default: result = 8'b00000000;
-endcase
+    case (alu_control)
+        2'b00: result = a + b;   // ADD
+        2'b01: result = a - b;   // SUB
+        2'b10: result = a & b;   // AND
+        2'b11: result = a | b;   // OR
+        default: result = 8'b0;
+    endcase
 end
 
 endmodule
