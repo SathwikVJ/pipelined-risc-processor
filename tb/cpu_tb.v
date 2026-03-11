b433a7b465112c90799c2bbe69c4e3106085a37a
SathwikVJ <sathwikvempaty@gmail.com>
2026-03-11 19:30:51 +0530

    Update cpu_tb.v

--- tb/cpu_tb.v
+++ tb/cpu_tb.v
@@ -1,27 +1,21 @@
 `timescale 1ns/1ps
-
 module cpu_tb;
 
 reg clk;
 reg reset;
 
 cpu_top uut(
-.clk(clk),
-.reset(reset)
+    .clk(clk),
+    .reset(reset)
 );
 
 initial begin
-
-$dumpfile("dump.vcd");
-$dumpvars(0,cpu_tb);
-
-clk = 0;
-reset = 1;
-
-#10 reset = 0;
-
-#200 $finish;
-
+    $dumpfile("dump.vcd");
+    $dumpvars(0, cpu_tb);
+    clk   = 0;
+    reset = 1;
+    #10 reset = 0;
+    #200 $finish;
 end
 
 always #5 clk = ~clk;
