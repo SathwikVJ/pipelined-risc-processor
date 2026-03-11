ffee2849d88402fbab489432385fd96ac67dc246
SathwikVJ <sathwikvempaty@gmail.com>
2026-03-11 19:30:12 +0530

    Update hazard_unit.v

--- rtl/hazard_unit.v
+++ rtl/hazard_unit.v
@@ -1,18 +1,16 @@
 module hazard_unit(
-input [2:0] id_rs1,
-input [2:0] id_rs2,
-input [2:0] ex_rd,
-input ex_regwrite,
-output reg stall
+    input  [2:0] id_rs1,
+    input  [2:0] id_rs2,
+    input  [2:0] ex_rd,
+    input        ex_regwrite,
+    output reg   stall
 );
 
 always @(*) begin
-
-if(ex_regwrite && ((ex_rd == id_rs1) || (ex_rd == id_rs2)))
-    stall = 1;
-else
-    stall = 0;
-
+    if (ex_regwrite && ((ex_rd == id_rs1) || (ex_rd == id_rs2)))
+        stall = 1;
+    else
+        stall = 0;
 end
 
 endmodule
