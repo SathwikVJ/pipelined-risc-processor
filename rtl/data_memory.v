a8ec65fe89ff8cf49f0fc7e9016815d7dede3d57
SathwikVJ <sathwikvempaty@gmail.com>
2026-03-11 17:17:39 +0530

    Update data_memory.v

--- rtl/data_memory.v
+++ rtl/data_memory.v
@@ -1,1 +1,18 @@
+module data_memory(
+input clk,
+input we,
+input [7:0] addr,
+input [7:0] write_data,
+output [7:0] read_data
+);
 
+reg [7:0] memory[255:0];
+
+assign read_data = memory[addr];
+
+always @(posedge clk) begin
+if(we)
+memory[addr] <= write_data;
+end
+
+endmodule
